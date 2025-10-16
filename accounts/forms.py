54660aee8887b8c9c26ea5488086919e88fb3f8f
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-10-16 23:29:34 +0900

    参考書P.228まで

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = {"username","password1","password2"}
+        fields = ["username","password1","password2"]
